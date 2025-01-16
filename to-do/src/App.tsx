938a888df8597d87bed4383b0569f80128b63924
marumarma <mariaborisovna2004@mail.ru>
2025-01-16 16:22:22 +0700

    Создан шаблон карточки
    Была создана карточка с задачей, на которой указаны заголовок, статус, описание, автор, дата создания и дата последнего редактирования, а также добавлена кнопка редактирования карточки.

--- to-do/src/App.tsx
+++ to-do/src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import {FC} from 'react';
+import 'antd/es/button/style';
+import { Card, Button, Typography, Tag } from 'antd';
+import { EditOutlined } from '@ant-design/icons';
 
-const App = () => {
-  return(
-    <div>
-      Init
+const { Title, Paragraph, Text} = Typography;
+
+const App: FC = () => {
+  return (
+    <div className="App">  
+      <Card style={{ width: 300 }}>
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <Title level={3} style={{ margin: 0 }}>Заголовок</Title>
+        <Button type="link" icon={<EditOutlined style={{ color: 'gray', fontSize: '20px' }}/>} key="edit"/>
+      </div>
+      <Tag color='green' style={{ marginTop: 8 }}>Статус</Tag>
+      <Paragraph style={{ marginTop: 16 }}>Описание ляляляляляляляляляляля аруцшкрукрцйгщкркйк</Paragraph>
+      <Text type="secondary">Автор: example@mail.ru</Text>
+      <br/>
+      <Text type="secondary">Дата создания: 12.12.2024</Text>
+      </Card>
     </div>
-  )
+  );
 }
 
 export default App;
--- to-do/src/index.tsx
+++ to-do/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx'
+import './index.css';
+import App from './App.tsx';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -8,3 +9,4 @@ const root = ReactDOM.createRoot(
 root.render(
     <App />
 );
+
